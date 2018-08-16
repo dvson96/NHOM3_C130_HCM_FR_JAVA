aea8a0711248c045c1a520f9e24ba37c93347d3b
Dong Van Son <dvson96.cdt@gmail.com>
2018-08-16 15:41:08 +0700

    update

--- SourceCode/src/main/java/com/team3/app/repositories/GroupCategoryRepository.java
+++ SourceCode/src/main/java/com/team3/app/repositories/GroupCategoryRepository.java
@@ -1,4 +1,4 @@
-package com.team3.app.repository;
+package com.team3.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
